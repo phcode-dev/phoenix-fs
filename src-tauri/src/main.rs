95f0b688c8dea6d819174199555342caa1f13ac7
abose <arun@core.ai>
2023-08-11 15:17:56 +0530

    feat: tauri test reporter will now log to console for github actions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,22 +3,12 @@ all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
 
-use tauri::api::process::Command;
 use tauri::GlobalWindowEvent;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(handle: tauri::AppHandle, name: &str) -> String {
-    let resource_path = handle.path_resolver()
-        .resolve_resource("app/hello.js")
-        .expect("failed to resolve resource");
-    Command::new_sidecar("phnode")
-        .expect("failed to create `my-sidecar` binary command")
-        .args(resource_path.as_path().to_str())
-        .spawn()
-        .expect("Failed to spawn sidecar");
-    println!("hello");
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn console_log(_handle: tauri::AppHandle, message: &str) {
+    println!("{}", message);
 }
 
 fn process_window_event(event: &GlobalWindowEvent) {
@@ -32,7 +22,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs_extra::init())
         .on_window_event(|event| process_window_event(&event))
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![console_log])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
