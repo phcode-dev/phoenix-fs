62fdee6c6fd91ef106271bc7caa1c6c7e6eb9d57
abose <arun@core.ai>
2023-09-17 20:27:43 +0530

    docs: for fs.watchAsync and fs.unwatchAsync APIs

--- src/fslib_watch.js
+++ src/fslib_watch.js
@@ -124,9 +124,40 @@ function reportCreateEvent(path, isDir) {
 }
 
 /**
- * @returns {EventEmitter}
- * @param {string} path The watched root
- * @param {string|Array<string>} gitIgnorePaths The contents of the gitIgnore file as text. The watcher will ignore all files matching git ignore.
+ * Watch a specific path asynchronously for filesystem changes.
+ *
+ * This function returns a promise that resolves an `EventEmitter` that will emit the following events:
+ * - `fs.WATCH_EVENTS.ADD_FILE`: When a file is created.
+ * - `fs.WATCH_EVENTS.ADD_DIR`: When a directory is created.
+ * - `fs.WATCH_EVENTS.UNLINK_FILE`: When a file is deleted.
+ * - `fs.WATCH_EVENTS.UNLINK_DIR`: When a directory is deleted.
+ * - `fs.WATCH_EVENTS.CHANGE`: When a file is changed.
+ *
+ * The watcher will ignore all files matching patterns in the provided gitignore.
+ *
+ * @function
+ * @async
+ * @param {string} path - The path to watch for filesystem changes.
+ * @param {string|Array<string>} [gitIgnorePaths=""] - The patterns to ignore, either provided as a string
+ *      (representing the content of a `.gitignore` file) or an array of individual patterns. The watcher will
+ *      adhere to the standard `.gitignore` specification as detailed at https://git-scm.com/docs/gitignore.
+ *      It's important to note that if a parent directory is excluded from watching, its child directories
+ *      will also be excluded, regardless of any `un-ignore` patterns in git ignore file(e.g., `!node_modules/dont_ignore_dir`).
+ *
+ * @returns {EventEmitter} - The event emitter that will notify of filesystem changes.
+ *
+ * @example
+ * // In the below watcher, we provide a gitignore formatted text to ignores 'node_modules' folder
+ * // See https://git-scm.com/docs/gitignore for details.
+ * const watcher = await fs.watchAsync('/path/to/watch', 'node_modules');
+ *
+ * watcher.on(Constants.WATCH_EVENTS.ADD_FILE, (event) => {
+ *   console.log(`File created: ${event.path}`);
+ * });
+ *
+ * watcher.on(Constants.WATCH_EVENTS.UNLINK_DIR, (event) => {
+ *   console.log(`Directory deleted: ${event.path}`);
+ * });
  */
 async function watchAsync(path, gitIgnorePaths="") {
     const eventEmitter = new EventEmitter();
@@ -136,13 +167,39 @@ async function watchAsync(path, gitIgnorePaths="") {
     return eventEmitter;
 }
 
-function unwatchAsync(eventEmitter) {
+/**
+ * Stops watching for filesystem changes on a previously set path.
+ *
+ * Once you've stopped watching using `unwatchAsync`, any further operations on the event emitter
+ * will throw an error. If you wish to start watching again, you will need to call `fs.watchAsync`.
+ *
+ * @function
+ * @param {EventEmitter} eventEmitter - The event emitter returned by `fs.watchAsync` that you wish to stop watching.
+ * @throws {Errors.EINVAL} Throws an error if the watcher is already closed or if operations are attempted after closing.
+ *
+ * @example
+ * const watcher = await fs.watchAsync('/path/to/watch', 'node_modules');
+ *
+ * // Listen to an event.
+ * watcher.on(fs.WATCH_EVENTS.ADD_FILE, (event) => {
+ *   console.log(`File created: ${event.path}`);
+ * });
+ *
+ * // ... After some time, stop watching.
+ * await unwatchAsync(watcher);
+ *
+ * // Throws error since the watcher is closed.
+ * watcher.on(fs.WATCH_EVENTS.ADD_FILE, (event) => {
+ *   console.log(`File created: ${event.path}`);
+ * });
+ */
+async function unwatchAsync(eventEmitter) {
     if(eventEmitter.allreadyClosed) {
         return ;
     }
     eventEmitter.removeAllListeners();
     eventEmitter.on = function () {
-        throw new Errors.EIO("The File watcher is closed. Please use `fs.watchAsync` if you want to watch again.");
+        throw new Errors.EINVAL("The File watcher is closed. Please use `fs.watchAsync` if you want to watch again.");
     };
     eventEmitter.allreadyClosed = true;
 
